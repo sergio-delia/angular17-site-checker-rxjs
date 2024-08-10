caeb6776d0960a11942506d44112925c2980efa2
Sergio <sergio.delia@yahoo.it>
2024-08-10 18:19:00 +0200

    added environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  API_KEY: "AIzaSyDeVPREzn_F1KsnLEHMBzAndD4mCy0Tuu0",
+  EMAILJS_KEY: "xl-SgHJTGhBmYD_e1",
+  firebaseConfig: {
+    projectId: 'site-checker-acb0f',
+    appId: '1:424586472592:web:7e8b03f03daf329001bc15',
+    storageBucket: 'site-checker-acb0f.appspot.com',
+    apiKey: 'AIzaSyDeVPREzn_F1KsnLEHMBzAndD4mCy0Tuu0',
+    authDomain: 'site-checker-acb0f.firebaseapp.com',
+    messagingSenderId: '424586472592',
+  }
+};
